95f5d0901431080129ae9aa893f7058fc3b63041
ayoubelguaje <77021285+ayoubelguaje@users.noreply.github.com>
2021-01-18 19:00:57 +0100

    add contactar page

---
+++ main.js
@@ -0,0 +1,6 @@
+var myForm = document.getElementsByClassName('contactar'),
+    myBtn = document.getElementById('send'),
+    newText ;
+    myBtn.onclick = function(){
+        myForm.textContent = 'kljkljkl';
+    }
